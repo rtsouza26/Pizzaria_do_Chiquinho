0a1d0e2035dbb43001f55d54414343326fba0d5b
Daniel-Melo97 <daniellemosmelo@hotmail.com>
2017-05-25 00:05:17 -0300

    feito

---
+++ Pizzaria_do_Chiquinho/src/principal/Pedido.java
@@ -0,0 +1,42 @@
+package principal;
+
+/**Classe para objetos do tipo Produtos, onde serão contidos, valores e métodos para o mesmo.
+ * @author 
+ * @version 1.0
+ * @since Release 0
+ */
+
+public class Pedido {
+	
+	private Cliente pessoa;
+	private string obs;
+	private String status;
+	private float total;
+	private Item_produto[] produtos;
+	
+	public String getObs() {
+		return obs;
+	}
+	public void setObs(String obs) {
+		this.obs = obs;
+	}
+	public float getTotal() {
+		return total;
+	}
+	public void setTotal(int valor) {
+		this.total = valor;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String situacao) {
+		this.status = situacao;
+	}
+	public Cliente getCliente(){
+		return pessoa;
+	}
+	public void setPessoa(Cliente pessoa){
+		this.pessoa=pessoa;
+	}
+	
+}
