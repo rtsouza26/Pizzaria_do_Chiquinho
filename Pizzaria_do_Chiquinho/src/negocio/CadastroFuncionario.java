b889a5abebdf5b5176f3dca5d2c4227e79054377
Daniel-Melo97 <daniellemosmelo@hotmail.com>
2017-07-04 01:44:03 -0300

    Implementação de remover e buscar em CadastroFuncionario

--- Pizzaria_do_Chiquinho/src/negocio/CadastroFuncionario.java
+++ Pizzaria_do_Chiquinho/src/negocio/CadastroFuncionario.java
@@ -26,6 +26,17 @@ public class CadastroFuncionario {
 				System.out.println("Funcionario já existe");
 			}
 		}
+	}//Daqui pra baixo foi eu que fiz, qualquer erro vcs me avisem, blz?
+	public Funcionario buscarFunc(String nome){
+		return this.funcBD.buscarFuncBD(nome);
 	}
+	public void removerFunc(String nome){
+		if(this.funcBD.removerFuncBD(nome)){
+			System.out.println("Funcionário deletado com sucesso");
+		}else{
+			System.out.println("Não foi possível deletar funcionário");
+		}
+	}
+	
 
 }
